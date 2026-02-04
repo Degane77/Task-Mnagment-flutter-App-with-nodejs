72d4ad70b5921d3ef4ecbf1c487a6069e21fe7d3
Salmaan Duraan <salmaanduraan3@gmail.com>
2026-02-04 15:16:04 +0300

    KAN-11 user model

--- task_managemant/lib/models/user_model.dart
+++ task_managemant/lib/models/user_model.dart
@@ -1,9 +1,19 @@
+/// User model representing an authenticated system user
 class UserModel {
-  String id;
-  String name;
-  String email;
-  String role;
+  /// Unique user ID (usually from backend like MongoDB)
+  final String id;
 
+  /// Full name of the user
+  final String name;
+
+  /// Email address of the user
+  final String email;
+
+  /// Role of the user (admin, manager, user)
+  final String role;
+
+  /// Constructor with required fields
+  /// Default role is set to 'user'
   UserModel({
     required this.id,
     required this.name,
@@ -11,6 +21,7 @@ class UserModel {
     this.role = 'user',
   });
 
+  /// Factory constructor to create UserModel from JSON
   factory UserModel.fromJson(Map<String, dynamic> json) {
     return UserModel(
       id: json['_id']?.toString() ?? '',
@@ -20,9 +31,18 @@ class UserModel {
     );
   }
 
+  /// Checks if user is an admin
   bool get isAdmin => role == 'admin';
+
+  /// Checks if user is a manager
   bool get isManager => role == 'manager';
+
+  /// Permission: can create tasks
   bool get canCreateTask => isAdmin || isManager;
+
+  /// Permission: can delete tasks
   bool get canDeleteTask => isAdmin || isManager;
+
+  /// Permission: can manage users
   bool get canManageUsers => isAdmin;
 }
